d0bf7a53c8831cc9fc8c4e0ea9061394418bf715
רחל לאה מנדלבוים <211954722@mbydomain>
2021-05-26 19:53:03 +0300

    client

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, {useEffect}from 'react';
 import SignIn from './component/signIn';
 import Home from './home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './component/login';
-import AddAsset from './component/addAsset';
-
+import SimpleAccordion from './component/simpleAccordion';
 
 function App() {
-  return (
 
+
+  return (
     <Router>
       <Switch>
         <Route path='/signin'>
           <SignIn />
         </Route>
         <Route path='/add'>
-          <AddAsset/>
+          <SimpleAccordion/>
         </Route>
         <Route path='/login'>
           <Login />
--- src/component/login.js
+++ src/component/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import './login.css'
-
+import {login} from '../API/loginApi';
 function Login(){
 
     const [userName, setuserName] = useState(0);
@@ -10,12 +10,21 @@ function Login(){
     setuserName(event.target.value);}
      function handleSubmit()
      {
+      
+  
+     }
+
+     function login1(){
+   
+      login();
 
      }
+     
     return(
         <form className='form' onSubmit={handleSubmit}>
         <TextField id="userName" label="שם משתמש" onChange={handleChange} variant="outlined" /><br />
         <TextField id="password" label="סיסמא" variant="outlined" /><br />
+        <button type="submit" onClick={login1}>כניסה</button>
         </form>
      
     );
